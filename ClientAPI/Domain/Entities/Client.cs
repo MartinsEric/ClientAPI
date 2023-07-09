9427212a27567df9f52216b4ec737e0d778bd521
Eric Martins <ericmartins.martins@gmail.com>
2023-07-09 19:45:23 -0300

    feat: add Client and PhoneNumber entities

---
+++ ClientAPI/Domain/Entities/Client.cs
@@ -0,0 +1,33 @@
+﻿namespace Domain.Entities
+{
+    internal class Client
+    {
+        public Guid Id { get; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public IEnumerable<PhoneNumber> Phones{ get; private set; }
+
+        public Client(string name, string email)
+        {
+            Id = Guid.NewGuid();
+            Phones = new List<PhoneNumber>();  
+            Name = name;   
+            Email = email;
+        }
+
+        public void UpdateEmail(string email)
+        {
+            this.Email = email;
+        }
+
+        public void UpdatePhoneNumber(PhoneNumber phoneNumber)
+        {
+            var phone = Phones.FirstOrDefault(p => p.ToString() == phoneNumber.ToString());
+            
+            if (phone != null)
+            {
+                phone.UpdatePhoneNumber(phoneNumber);
+            }
+        }
+    }
+}
---
+++ ClientAPI/Domain/Entities/PhoneNumber.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Enums;
+using System.Data;
+using System.Reflection.PortableExecutable;
+
+namespace Domain.Entities
+{
+    internal class PhoneNumber
+    {
+        public string DDD { get; private set; }
+        public string Number { get; private set; }
+        public PhoneType Type { get; private set; }
+
+        public PhoneNumber(string ddd, string number, PhoneType type)
+        {
+            DDD = ddd;
+            Number = number;
+            Type = type;
+        }
+
+        public void UpdatePhoneNumber(PhoneNumber phoneNumber)
+        {
+            DDD = phoneNumber.DDD;
+            Number = phoneNumber.Number;    
+            Type = phoneNumber.Type;
+        }
+
+        public override string ToString()
+        {
+            return DDD + Number;
+        }
+    }
+}
