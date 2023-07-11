58239a92ce0dc8da2039d40cc55b3ee1327c5211
Eric Martins <ericmartins.martins@gmail.com>
2023-07-11 11:34:32 -0300

    test: add validation to ensure that cannot have two clients with the same email

--- ClientAPI/Tests/UseCases/AddClientUseCaseTests.cs
+++ ClientAPI/Tests/UseCases/AddClientUseCaseTests.cs
@@ -4,6 +4,7 @@ using Domain.Interfaces.Repositories;
 using Domain.Enums;
 using Moq;
 using Domain.DTOs;
+using Domain.Exceptions;
 
 namespace Tests.UseCases
 {
@@ -22,6 +23,24 @@ namespace Tests.UseCases
             await useCase.Execute(clientDTO);
 
             clientRepositoryMock.Verify(r => r.Add(It.IsAny<Client>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task Execute_CLientAlreadyExists_ThrowAlreadyExistsClientException()
+        {
+            var clientRepositoryMock = new Mock<IClientRepository>();
+            var useCase = new AddClientUseCase(clientRepositoryMock.Object);
+            var phone = new AddPhoneNumberDTO("021", "999999999", PhoneType.Mobile);
+            var phoneList = new List<AddPhoneNumberDTO> { phone };
+            var clientDTO = new AddClientDTO("Bruce Wayne", "notbatman@dc.com", phoneList);
+            var client = clientDTO.Transform();
+            var client2 = new Client("Jhon Doe", client.Email);
+
+            clientRepositoryMock.Setup(r => r.GetByEmail(client2.Email)).ReturnsAsync(client2);
+
+            await Assert.ThrowsAsync<AlreadyExistsClientExcption>(() => useCase.Execute(clientDTO));
+
+            clientRepositoryMock.Verify(r => r.Add(It.IsAny<Client>()), Times.Never);
         }
     }
 }
