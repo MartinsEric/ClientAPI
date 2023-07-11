ce60d2ca6bacf6fa84933ff5a5da5a7e2ad02c0c
Eric Martins <ericmartins.martins@gmail.com>
2023-07-11 00:40:35 -0300

    chore: config database connection and regiter repositories dependency injection

--- ClientAPI/WebAPI/Program.cs
+++ ClientAPI/WebAPI/Program.cs
@@ -1,5 +1,9 @@
 using Application.UseCases;
+using Domain.Interfaces.Repositories;
 using Domain.Interfaces.UseCases;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +20,10 @@ builder.Services.AddScoped<IAddClientUseCase, AddClientUseCase>();
 builder.Services.AddScoped<IUpdateClientEmailUseCase, UpdateClientEmailUseCase>();
 builder.Services.AddScoped<IUpdateClientPhoneUseCase, UpdateClientPhoneUseCase>();
 builder.Services.AddScoped<IDeleteClientUseCase, DeleteClientUseCase>();
+builder.Services.AddScoped<IClientRepository, ClientRepository>();
+builder.Services.AddScoped<IPhoneNumberRepository, PhoneNumberRepository>();
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("client_api"));
 
 var app = builder.Build();
 
