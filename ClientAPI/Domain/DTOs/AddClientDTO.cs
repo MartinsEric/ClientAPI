2952e71321b546d46b02ab4a218517a50c82e82a
Eric Martins <ericmartins.martins@gmail.com>
2023-07-11 00:30:17 -0300

    feat: create DTOs to add new client

---
+++ ClientAPI/Domain/DTOs/AddClientDTO.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+
+namespace Domain.DTOs
+{
+    public class AddClientDTO
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public ICollection<AddPhoneNumberDTO> Phones { get; set; }
+
+        public AddClientDTO(string name, string email, ICollection<AddPhoneNumberDTO> phones)
+        {
+            Name = name;
+            Email = email;
+            Phones = phones;
+        }
+
+        public Client Transform()
+        {
+            var client = new Client(Name, Email);
+            Phones.ToList().ForEach(p => client.AddPhoneNumber(new PhoneNumber(p.DDD, p.Number, p.Type)));
+
+            return client;
+        }
+    }
+}
---
+++ ClientAPI/Domain/DTOs/AddPhoneNumberDTO.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Enums;
+
+namespace Domain.DTOs
+{
+    public class AddPhoneNumberDTO
+    {
+        public string DDD { get; set; }
+        public string Number { get; set; }
+        public PhoneType Type { get; set; }
+
+        public AddPhoneNumberDTO(string ddd, string number, PhoneType type)
+        {
+            DDD = ddd;
+            Number = number;
+            Type = type;
+        }
+    }
+}
